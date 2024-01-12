207e3ee2777c45b4770bc51654effc33f9f85572
LEEJIWOOE <sk9311@naver.com>
2024-01-12 18:22:39 +0900

    JS/CSS 파일 분리하여 다시 저장합니다.

---
+++ section_trafic/js/trafic.js
@@ -0,0 +1,15 @@
+
+var trafic_slide_gallery_Xoffset = 0
+function trafic_Move(){
+    trafic_slide_gallery_Xoffset -= 300
+    if (trafic_slide_gallery_Xoffset < -2400 ){
+        trafic_slide_gallery_Xoffset = 0
+    }
+    $('#trafic_slide_gallery').animate({'marginLeft':`${trafic_slide_gallery_Xoffset}px`},300)
+}
+
+
+// $(String_Selector)  : 요소 찾아서 돌려줌
+// $(String_HTML)  : 입력받은 HTML 코드를 지가 새롭게 생성해서 돌려줌.
+// $(function) : 페이지 로드 완료됐을 떄 해당 함수 돌리라고 등록해줌
+// $(element) : 해당 요소를 jquery 객체로 만들어줌
