d33ff02b9a1ea60c338ff51802752e6b8f6abbd1
LEEJIWOOE <sk9311@naver.com>
2024-01-15 11:15:25 +0900

    뒤로가기 버튼 구현했습니다.

--- section_trafic/js/trafic.js
+++ section_trafic/js/trafic.js
@@ -1,4 +1,9 @@
 
+// $(String_Selector)  : 요소 찾아서 돌려줌
+// $(String_HTML)  : 입력받은 HTML 코드를 지가 새롭게 생성해서 돌려줌.
+// $(function) : 페이지 로드 완료됐을 떄 해당 함수 돌리라고 등록해줌
+// $(element) : 해당 요소를 jquery 객체로 만들어줌
+
 var trafic_slide_gallery_Xoffset = 0
 function trafic_Move(){
     trafic_slide_gallery_Xoffset -= 300
@@ -7,9 +12,11 @@ function trafic_Move(){
     }
     $('#trafic_slide_gallery').animate({'marginLeft':`${trafic_slide_gallery_Xoffset}px`},300)
 }
+function trafic_back() {
+    trafic_slide_gallery_Xoffset += 300
+    if (trafic_slide_gallery_Xoffset > 0){
+        trafic_slide_gallery_Xoffset = -2400
+    }
+    $(`#trafic_slide_gallery`).animate({'marginLeft':`${trafic_slide_gallery_Xoffset}px`},300)
+}
 
-
-// $(String_Selector)  : 요소 찾아서 돌려줌
-// $(String_HTML)  : 입력받은 HTML 코드를 지가 새롭게 생성해서 돌려줌.
-// $(function) : 페이지 로드 완료됐을 떄 해당 함수 돌리라고 등록해줌
-// $(element) : 해당 요소를 jquery 객체로 만들어줌
