3ac37e08f40a674e27abe5d67c34e0c604b46837
LEEJIWOOE <sk9311@naver.com>
2024-01-15 14:58:22 +0900

    재생 중지 버튼 만들었습니다

--- section_trafic/js/trafic.js
+++ section_trafic/js/trafic.js
@@ -4,7 +4,8 @@
 // $(function) : 페이지 로드 완료됐을 떄 해당 함수 돌리라고 등록해줌
 // $(element) : 해당 요소를 jquery 객체로 만들어줌
 
-var trafic_slide_gallery_Xoffset = 0
+var trafic_slide_gallery_Xoffset = 0;
+var intervalId;
 function trafic_Move(){
     trafic_slide_gallery_Xoffset -= 300
     if (trafic_slide_gallery_Xoffset < -2400 ){
@@ -12,6 +13,7 @@ function trafic_Move(){
     }
     $('#trafic_slide_gallery').animate({'marginLeft':`${trafic_slide_gallery_Xoffset}px`},300)
 }
+
 function trafic_back() {
     trafic_slide_gallery_Xoffset += 300
     if (trafic_slide_gallery_Xoffset > 0){
@@ -20,3 +22,12 @@ function trafic_back() {
     $(`#trafic_slide_gallery`).animate({'marginLeft':`${trafic_slide_gallery_Xoffset}px`},300)
 }
 
+function trafic_play() {
+    // setInterval의 ID를 저장하고 반환
+    intervalId = setInterval(trafic_Move, 1000);
+}
+
+function trafic_stop() {
+    // 저장한 setInterval의 ID를 활용하여 clearInterval 호출
+    clearInterval(intervalId);
+}
