2dc6c08a32a1dba4d0944c9ffe0802667418847d
RBS <ttuleula@gmail.com>
2024-01-17 21:10:53 +0900

    차트 크기 맞추는 중 | 중간 저장

--- section_weatherChart/js/weatherChart.js
+++ section_weatherChart/js/weatherChart.js
@@ -1,97 +1,108 @@
 // jquery plugin
-$(function() {
-    $('.slide_gallery').bxSlider({
-        auto: false,
-        autoControls: false,
-        pagerCustom: '.slider-pager'
-    });
+$(function () {
+    //초기 업데이트
+    CreateWeatherChart()
+    weeklyUpdate();
 });
+
 // 차트 변수 선언
-const TEMP_PRICES = [];
+let weaterChartData = [];
 let tempChart;
 
-// 차트 초기화
-const ctx = document.getElementById('tempChart').getContext('2d');
-tempChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-        datasets: [{
-            label: '주간 날시 기온차',
-            data: TEMP_PRICES,
-            borderWidth: 1,
-            fill: true,
-        }],
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
+const MIN_INTERVAL = 500;
+const MAX_INTERVAL = 2000;
 
-// 랜덤 값 생성
-let random_sunnyChange = (Math.random() - 20) * 10;
-let random_cloudyChange = (Math.random() - 20) * 8;
-let random_rainyChange = (Math.random() - 20) * 6;
-let random_windyChange = (Math.random() - 20) * 4;
-let random_snowyChange = (Math.random() - 20) * 2;
-
-// 램덤 값 자동 반복 생성
-function updateTemp(){
-    // 변동 및 변동률 업데이트
-    // const currentTemp = parseFloat($(".change").text().repl);
-    let sunnyTempChange = Math.random(random_sunnyChange / 10);
-    let cloudyTempChange = Math.random(random_cloudyChange / 10);
-    let rainyTempChange = Math.random(random_rainyChange / 100);
-    let windyTempChange = Math.random(random_windyChange / 100);
-    let snowyTempChange = Math.random(random_snowyChange / 10);
-
-    // 온도 배열에 추가
-    TEMP_PRICES.push(sunnyTempChange);
-    TEMP_PRICES.push(cloudyTempChange);
-    TEMP_PRICES.push(rainyTempChange);
-    TEMP_PRICES.push(windyTempChange);
-    TEMP_PRICES.push(snowyTempChange);
-
-    // 최대 7개까지 유지
-    if (TEMP_PRICES.length > 7){
-        TEMP_PRICES.shift()	// 첫번째 원소 제거
-    }
-    // 차트 업데이트
-    updateChart();
-
-    // 변동 및 변동률 업데이트
-    $('.change_sunnyDay').text(sunnyTempChange.toFixed(1) + '°');
-    $('.change_cloudyDay').text(cloudyTempChange.toFixed(1) + '°');
-    $('.change_rainyDay').text(rainyTempChange.toFixed(1) + '°');
-    $('.change_windyDay').text(windyTempChange.toFixed(1) + '°');
-    $('.change_snowyDay').text(snowyTempChange.toFixed(1) + '°');
+const weatherChartData = {
+    '맑음': [14, 24, 'src/brightness.png'],
+    '흐림': [10, 13, 'src/sun.png'],
+    '비': [5, 8, "src/rain.png"],
+    '눈': [-5, 0, "src/snow.png"],
+    '바람': [0, 20, "src/wind.png"]
 }
 
-//초기 업데이트
-updateTemp();
 
-/* chart start */
-/* random timeout */
-const MIN_INTERVAL = 1000; // 최소 1초
-const MAX_INTERVAL = 3000; // 최대 3초
+function getRandomWeather() {
+    return weatherChartData[Object.keys(weatherChartData)[Math.floor(getRandomInterval(0, 5))]]
+}
 
-function weeklyUpdate(){
-    const randomInterval = Math.random() * (MAX_INTERVAL - MIN_INTERVAL) + MIN_INTERVAL;
-    setTimeout(function (){
-        updateTemp();
+function getRandomInterval(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function weeklyUpdate() {
+    const randomInterval = getRandomInterval(MIN_INTERVAL, MAX_INTERVAL)
+    setTimeout(function () {
+        updateTempByRendom();
         weeklyUpdate();
     }, randomInterval);
 }
-weeklyUpdate();
 
-/* chart temp */
-function updateChart(){
-    tempChart.data.label = Array.from({length: 7}, (_, i) => '');	// 데이터 레이블 비우기
-    tempChart.data.label.push('');
-    tempChart.data.datasets[0].data = TEMP_PRICES;
+// 차트 초기화
+function CreateWeatherChart() {
+    let weatherChart = $('#weatherChart')
+    let gallery_wrap = $(`<div id='gallery_wrap' ><div class="slide_gallery"></div></div>`)
+    let weatherChart_sec = $(`<div id='weatherChart_sec' ><canvas id="tempChart"></canvas></div>`)
+
+    weatherChart.append(gallery_wrap)
+    weatherChart.append(weatherChart_sec)
+
+    // table 생성
+    let table = $(`<table class="weatherChart_table"></table>`)
+    let tr2 = $(`<tr></tr>`)
+    let tr3 = $(`<tr></tr>`)
+    gallery_wrap.append(table)
+    table.append($(`<tr><th>월</th><th>화</th><th>수</th><th>목</th><th>금</th><th>토</th><th>일</th></tr>`))
+    table.append(tr2)
+    table.append(tr3)
+
+    for (let i = 0; i < 7; i++) {
+        const [minTemp, maxTemp, ImgSrc] = getRandomWeather();
+
+        let randTemp = getRandomInterval(minTemp, maxTemp).toFixed(1);
+        weaterChartData.push(randTemp);
+        tr2.append($(`<td><img src="${ImgSrc}"></td>`))
+        tr3.append($(`<td>${randTemp}°</td>`))
+    }
+
+    // 차트 생성
+    let ctx = document.getElementById('tempChart').getContext('2d');
+    tempChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+            datasets: [{
+                label: '주간 날시 기온차',
+                data: weaterChartData,
+                borderWidth: 1,
+                fill: true,
+            }],
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+
+function updateTempByRendom() {
+    weaterChartData = []
+    for (let i = 1; i < 8; i++) {
+        const [minTemp, maxTemp, ImgSrc] = getRandomWeather();
+
+        let randTemp = getRandomInterval(minTemp, maxTemp).toFixed(1);
+        ;
+        weaterChartData.push(randTemp);
+
+        // 그림 바꿔치기
+        $(`#gallery_wrap tr:nth-child(2) td:nth-child(${i}) img`).attr('src', ImgSrc)
+
+        // 날씨 바꿔치기
+        $(`#gallery_wrap tr:nth-child(3) td:nth-child(${i})`).text(randTemp)
+    }
+
+    tempChart.data.datasets[0].data = weaterChartData;
     tempChart.update();
 }
